3c31dc91c030040db30a53f2a639e6e3c0af0348
n3vsk1y <alexafanasenko900@gmail.com>
2024-12-19 00:35:26 +0300

    intro

--- main_screen.gd
+++ main_screen.gd
@@ -4,6 +4,8 @@ var button_type = null
 
 func _on_ready() -> void:
 	MusicManager.play_music()
+	$intro_out/AnimationPlayer.play("intro_out")
+	$intro_out/Timer.start()
 
 
 func _on_start_pressed() -> void:
@@ -24,3 +26,7 @@ func _on_exit_pressed() -> void:
 func _on_timer_timeout() -> void:
 	if button_type == "start":
 		get_tree().change_scene_to_file("res://node_3d.tscn")
+
+
+func _on_intro_timer_timeout() -> void:
+	$intro_out.visible = false
