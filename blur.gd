223ef36b97c97d4107ff1f5e292426175e546d39
n3vsk1y <alexafanasenko900@gmail.com>
2024-12-24 01:20:53 +0300

    pause + inventory

--- blur.gd
+++ blur.gd
@@ -5,3 +5,6 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "blur_out":
 		var blur = get_parent().get_node("Blur")
 		blur.visible = false
+		if get_parent().has_node("CanvasLayer/Control/pause"):
+			get_parent().get_node("CanvasLayer/Control/pause").visible = true
+			get_parent().get_node("CanvasLayer/Control/inventory").visible = true
--- node_3d.gd
+++ node_3d.gd
@@ -9,12 +9,34 @@ func _ready() -> void:
 	
 	$Blur.visible = false
 
+func _process(delta: float) -> void:
+	Btns()
 
-func _on_timer_timeout() -> void:
-	$intro_out.visible = false
+func Btns():
+	if Input.is_action_just_pressed("escape") and !$Blur.visible:
+		_on_pause_pressed()
+	elif Input.is_action_just_pressed("escape") and $Blur.visible:
+		$CanvasLayer/Control/PauseMenu._on_back_pressed()
+	elif Input.is_action_just_pressed("inventory") and !$Blur.visible:
+		_on_inventory_pressed()
+	elif Input.is_action_just_pressed("inventory") and $Blur.visible:
+		$Blur/AnimationPlayer.play("blur_out")
+		$CanvasLayer/Control/Inventory/AnimationPlayer.play("out")
 
 
 func _on_pause_pressed() -> void:
-	$Blur.visible = true
-	$Blur/AnimationPlayer.play('blur_in')
-	$CanvasLayer/Control/PauseMenu/AnimationPlayer.play("in")
+	if !$Blur.visible:
+		$Blur.visible = true
+		$Blur/AnimationPlayer.play("blur_in")
+		$CanvasLayer/Control/PauseMenu/AnimationPlayer.play("in")
+		$CanvasLayer/Control/pause.visible = false
+		$CanvasLayer/Control/inventory.visible = false
+
+
+func _on_inventory_pressed() -> void:
+	if !$Blur.visible:
+		$Blur.visible = true
+		$Blur/AnimationPlayer.play("blur_in")
+		$CanvasLayer/Control/Inventory/AnimationPlayer.play("in")
+		$CanvasLayer/Control/pause.visible = false
+		$CanvasLayer/Control/inventory.visible = false
