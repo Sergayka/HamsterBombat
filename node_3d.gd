939bd9463f09a33d4f6c5b95842c0e9e3eafe1c4
sergayka <xuverthebest@mail.ru>
2024-12-24 01:49:13 +0300

    add boy

--- node_3d.gd
+++ node_3d.gd
@@ -12,22 +12,20 @@ extends Node3D
 	#music_player.play()  # Музыка начнёт играть сразу после загрузки сцены
 
 
-func _ready() -> void:
-	$intro_out/AnimationPlayer.play("intro_out")
-	$intro_out/Timer.start()
-	
-	MusicManager.toggle_music()
-	
-	$Blur.visible = false
-
-
-func _on_timer_timeout() -> void:
-	$intro_out.visible = false
-
-
-func _on_pause_pressed() -> void:
-	$Blur.visible = true
-	$Blur/AnimationPlayer.play('blur_in')
-	$CanvasLayer/Control/PauseMenu/AnimationPlayer.play("in")
-
-
+#func _ready() -> void:
+	#$intro_out/AnimationPlayer.play("intro_out")
+	#$intro_out/Timer.start()
+	#
+	#MusicManager.toggle_music()
+	#
+	#$Blur.visible = false
+#
+#
+#func _on_timer_timeout() -> void:
+	#$intro_out.visible = false
+#
+#
+#func _on_pause_pressed() -> void:
+	#$Blur.visible = true
+	#$Blur/AnimationPlayer.play('blur_in')
+	#$CanvasLayer/Control/PauseMenu/AnimationPlayer.play("in")
